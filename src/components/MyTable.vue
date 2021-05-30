e7eeedbbbbf95311c15b8d8cb6c6faf0a7d4b3e7
oksana-1 <4438488@gmail.com>
2021-05-30 16:35:50 +0300

    General dummy-table markup

---
+++ src/components/MyTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <tbody>
+      <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+        <th>Header 3</th>
+        <th>Header 4</th>
+        <th>Header 5</th>
+      </tr>
+      <tr>
+        <td>Row:1 Cell:1</td>
+        <td>Row:1 Cell:2</td>
+        <td>Row:1 Cell:3</td>
+        <td>Row:1 Cell:4</td>
+        <td>Row:1 Cell:5</td>
+      </tr>
+      <tr>
+        <td>Row:2 Cell:1</td>
+        <td>Row:2 Cell:2</td>
+        <td>Row:2 Cell:3</td>
+        <td>Row:2 Cell:4</td>
+        <td>Row:2 Cell:5</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyTable",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
