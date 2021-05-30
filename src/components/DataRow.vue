0cd20b91956bbd003ae965cc41105235ebbdcb51
oksana-1 <4438488@gmail.com>
2021-05-30 16:53:44 +0300

    Component for data-row

---
+++ src/components/DataRow.vue
@@ -0,0 +1,22 @@
+<template>
+  <tr>
+    <td>{{ item.prop1}}</td>
+    <td>{{ item.prop2 }}</td>
+    <td>{{ item.prop3 }}</td>
+    <td>{{ item.prop4 }}</td>
+    <td>{{ item.prop5 }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "DataRow",
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -9,31 +9,47 @@
         <th>Header 4</th>
         <th>Header 5</th>
       </tr>
-      <tr>
-        <td>Row:1 Cell:1</td>
-        <td>Row:1 Cell:2</td>
-        <td>Row:1 Cell:3</td>
-        <td>Row:1 Cell:4</td>
-        <td>Row:1 Cell:5</td>
-      </tr>
-      <tr>
-        <td>Row:2 Cell:1</td>
-        <td>Row:2 Cell:2</td>
-        <td>Row:2 Cell:3</td>
-        <td>Row:2 Cell:4</td>
-        <td>Row:2 Cell:5</td>
-      </tr>
+      <DataRow
+        v-for="(item, i) in dummy"
+        :key="`dummy-${i}`"
+        :item="item"
+      />
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
+import DataRow from "./DataRow";
 export default {
   name: "MyTable",
+  components: {DataRow},
   data() {
     return {
+      dummy: [
+        {
+          prop1: "Row:1 Cell:1",
+          prop2: "Row:1 Cell:2",
+          prop3: "Row:1 Cell:3",
+          prop4: "Row:1 Cell:4",
+          prop5: "Row:1 Cell:5",
+        },
+        {
+          prop1: "Row:2 Cell:1",
+          prop2: "Row:2 Cell:2",
+          prop3: "Row:2 Cell:3",
+          prop4: "Row:2 Cell:4",
+          prop5: "Row:2 Cell:5",
+        },
+        {
+          prop1: "Row:3 Cell:1",
+          prop2: "Row:3 Cell:2",
+          prop3: "Row:3 Cell:3",
+          prop4: "Row:3 Cell:4",
+          prop5: "Row:3 Cell:5",
+        },
 
+      ]
     }
   }
 }
